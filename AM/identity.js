746e8934f316c5208e3a9b5b7e3da3cf52370542
sebastianpennerba-arch <sebastian.penner.ba@gmail.com>
2025-11-19 05:42:31 +0100

    Delete AM directory

--- AM/identity.js
+++
@@ -1,9 +0,0 @@
-function showLogin() {
-  document.getElementById("login-screen").style.display = "block";
-  document.getElementById("dashboard-screen").style.display = "none";
-}
-
-function showDashboard() {
-  document.getElementById("login-screen").style.display = "none";
-  document.getElementById("dashboard-screen").style.display = "block";
-}
