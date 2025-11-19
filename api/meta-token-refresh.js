bb39d567d191acf911b4a0d39d6552545a30405f
sebastianpennerba-arch <sebastian.penner.ba@gmail.com>
2025-11-19 20:21:32 +0100

    Update meta-token-refresh.js

--- api/meta-token-refresh.js
+++ api/meta-token-refresh.js
@@ -1,15 +1,22 @@
+// /api/meta-token-refresh.js
+
 export default async function handler(req, res) {
-  const { shortToken } = JSON.parse(req.body);
+  try {
+    const { shortToken } = JSON.parse(req.body);
 
-  const url =
-    `https://graph.facebook.com/v19.0/oauth/access_token?` +
-    `grant_type=fb_exchange_token&` +
-    `client_id=${process.env.META_APP_ID}&` +
-    `client_secret=${process.env.META_APP_SECRET}&` +
-    `fb_exchange_token=${shortToken}`;
+    const url =
+      `https://graph.facebook.com/v19.0/oauth/access_token?` +
+      `grant_type=fb_exchange_token&` +
+      `client_id=${process.env.META_APP_ID}&` +
+      `client_secret=${process.env.META_APP_SECRET}&` +
+      `fb_exchange_token=${shortToken}`;
 
-  const r = await fetch(url);
-  const data = await r.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-  res.status(200).json(data);
+    return res.status(200).json(data);
+  } catch (e) {
+    console.error("token refresh error:", e);
+    return res.status(500).json({ error: "exception" });
+  }
 }
