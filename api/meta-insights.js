fd952fce93e3e7b20653523d7b134b6c181bde49
sebastianpennerba-arch <sebastian.penner.ba@gmail.com>
2025-11-19 06:30:07 +0100

    Update meta-insights.js

--- api/meta-insights.js
+++ api/meta-insights.js
@@ -1,12 +1,26 @@
 export default async function handler(req, res) {
   const { token, accountId } = JSON.parse(req.body);
 
-  const url = `https://graph.facebook.com/v19.0/act_${accountId}/insights` +
-    `?fields=impressions,clicks,spend,ctr,cpc,roas,actions` +
-    `&date_preset=yesterday`;
+  const fields = [
+    "impressions",
+    "clicks",
+    "spend",
+    "reach",
+    "ctr",
+    "cpc",
+    "conversions",
+    "actions",
+    "action_values",
+    "conversion_values",
+    "purchase_roas"
+  ].join(",");
 
-  const apiRes = await fetch(url + `&access_token=${token}`);
-  const data = await apiRes.json();
+  const url =
+    `https://graph.facebook.com/v19.0/act_${accountId}/insights?` +
+    `fields=${fields}&date_preset=yesterday&access_token=${token}`;
+
+  const r = await fetch(url);
+  const data = await r.json();
 
   res.status(200).json(data);
 }
