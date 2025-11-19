9e11206deb32843aebb431b935c4796d4bdf545d
sebastianpennerba-arch <sebastian.penner.ba@gmail.com>
2025-11-19 19:10:21 +0100

    Create mock-insights.js

---
+++ api/mock-insights.js
@@ -0,0 +1,13 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    data: [{
+      impressions: "152340",
+      clicks: "4230",
+      ctr: "2.78",
+      spend: "98.52",
+      actions: [{ action_type: "purchase", value: "14" }],
+      action_values: [{ action_type: "purchase", value: "362.00" }],
+      purchase_roas: [{ value: "3.67" }]
+    }]
+  });
+}
