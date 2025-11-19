1186503162dc0a1aef42fc8d32578e7565393f9e
sebastianpennerba-arch <sebastian.penner.ba@gmail.com>
2025-11-19 06:28:47 +0100

    Update meta-auth.js

--- api/meta-auth.js
+++ api/meta-auth.js
@@ -3,18 +3,16 @@ export default async function handler(req, res) {
 
   const redirectUri = "https://amaschine.vercel.app/api/meta-auth";
 
-  const tokenUrl = `https://graph.facebook.com/v19.0/oauth/access_token?` +
-    `client_id=${process.env.META_APP_ID}` +
-    `&redirect_uri=${redirectUri}` +
-    `&client_secret=${process.env.META_APP_SECRET}` +
-    `&code=${code}`;
+  const url =
+    `https://graph.facebook.com/v19.0/oauth/access_token?` +
+    `client_id=${process.env.META_APP_ID}&` +
+    `redirect_uri=${redirectUri}&` +
+    `client_secret=${process.env.META_APP_SECRET}&` +
+    `code=${code}`;
 
-  const tokenRes = await fetch(tokenUrl);
-  const tokenData = await tokenRes.json();
+  const response = await fetch(url);
+  const tokenData = await response.json();
 
-  // Token speichern zu Clerk User
-  // (Client sendet Auth-Request)
-  
   const html = `
     <script>
       window.opener.postMessage(${JSON.stringify(tokenData)}, "*");
@@ -22,6 +20,6 @@ export default async function handler(req, res) {
     </script>
   `;
 
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader("Content-Type", "text/html");
   res.status(200).send(html);
 }
