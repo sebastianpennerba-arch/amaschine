dd6cf9c7200bc44f9f75e76c6675a5e4e16ad10d
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 15:26:46 +0100

    Delete packages/creativeLibrary/creativeLibrary.group.js

--- packages/creativeLibrary/creativeLibrary.group.js
+++
@@ -1,8 +0,0 @@
-// packages/creativeLibrary/creativeLibrary.group.js
-// Gruppierungslogik (nach Creative, Ad Name, Headline etc.) – Stub.
-
-export function groupCreatives(creatives, _groupByKey) {
-    if (!Array.isArray(creatives)) return {};
-    // Später: echte Grouping-Implementierung.
-    return { all: creatives };
-}
