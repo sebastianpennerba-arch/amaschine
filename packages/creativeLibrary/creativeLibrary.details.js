639ec6509ca4d6e782ae5d517269ab25ca5a3dcc
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:00:43 +0100

    Create creativeLibrary.details.js

---
+++ packages/creativeLibrary/creativeLibrary.details.js
@@ -0,0 +1,7 @@
+// packages/creativeLibrary/creativeLibrary.details.js
+// Detail-/Modal-Logik für einzelne Creatives (Stub).
+
+export function openCreativeDetailModal(_creative) {
+    // Wird später mit dem Modal-System verdrahtet.
+    console.debug("[CreativeLibrary] openCreativeDetailModal()");
+}
