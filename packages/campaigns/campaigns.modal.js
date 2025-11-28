8b498259ad0fe4eff4aab6cb8ac7b817a5887f22
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:02:31 +0100

    Create campaigns.modal.js

---
+++ packages/campaigns/campaigns.modal.js
@@ -0,0 +1,6 @@
+// packages/campaigns/campaigns.modal.js
+// Kampagnen-Detailmodal und Deep Dive – Stub.
+
+export function openCampaignDetailModal(_campaign) {
+    console.debug("[Campaigns] openCampaignDetailModal()");
+}
