ce371232672bf3b947151c2fda3a40eacca3d810
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:02:15 +0100

    Create campaigns.actions.js

---
+++ packages/campaigns/campaigns.actions.js
@@ -0,0 +1,10 @@
+// packages/campaigns/campaigns.actions.js
+// Aktionen wie "Budget erhöhen", "Pausieren", "Duplizieren" – Stub.
+
+export function pauseCampaign(_campaignId) {
+    console.debug("[Campaigns] pauseCampaign()");
+}
+
+export function increaseCampaignBudget(_campaignId, _amount) {
+    console.debug("[Campaigns] increaseCampaignBudget()");
+}
