95b623df9acd083ff5354ce4abe5433358d647f6
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:00:03 +0100

    Create creativeLibrary.render.js

---
+++ packages/creativeLibrary/creativeLibrary.render.js
@@ -0,0 +1,6 @@
+// packages/creativeLibrary/creativeLibrary.render.js
+// Placeholder Renderer für die Creative Library.
+
+export function renderCreativeLibrary(_options = {}) {
+    // Hier kommt später die echte Grid/List-Logik rein.
+}
