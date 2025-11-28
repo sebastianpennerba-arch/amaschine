f49b9e6d77ef36f053c25f1a052874c5c177e0cf
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:01:22 +0100

    Create creativeLibrary.demo.js

---
+++ packages/creativeLibrary/creativeLibrary.demo.js
@@ -0,0 +1,6 @@
+// packages/creativeLibrary/creativeLibrary.demo.js
+// Demo-Datenstruktur für Creative Library (Stub).
+
+export const DemoCreatives = [
+    // Später: realistische Demo-Creatives nach deinem DatAds-Kill-Konzept.
+];
