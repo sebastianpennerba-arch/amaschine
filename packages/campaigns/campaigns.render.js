c7eadbd663a30cf3f064d4fb16652aad26a497a4
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:02:00 +0100

    Create campaigns.render.js

---
+++ packages/campaigns/campaigns.render.js
@@ -0,0 +1,6 @@
+// packages/campaigns/campaigns.render.js
+// Rendering-Stub für die Kampagnentabelle.
+
+export function renderCampaignsTable(_campaigns, _options = {}) {
+    // Später: Rendern in #campaignsTableBody
+}
