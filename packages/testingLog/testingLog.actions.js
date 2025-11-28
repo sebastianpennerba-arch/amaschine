f79348264d46ef27e6d57e914437ec250605df4b
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:07:01 +0100

    Create testingLog.actions.js

---
+++ packages/testingLog/testingLog.actions.js
@@ -0,0 +1,10 @@
+// packages/testingLog/testingLog.actions.js
+// Aktionen: neuen Test anlegen, Status ändern, etc. – Stub.
+
+export function createTestEntry(_payload) {
+    console.debug("[TestingLog] createTestEntry()");
+}
+
+export function updateTestStatus(_id, _status) {
+    console.debug("[TestingLog] updateTestStatus()");
+}
