ad96041031f75ffdef18cac5e8af14baa5593e0e
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 10:06:44 +0100

    Create testingLog.render.js

---
+++ packages/testingLog/testingLog.render.js
@@ -0,0 +1,6 @@
+// packages/testingLog/testingLog.render.js
+// Rendering des Testing Logs – Stub.
+
+export function renderTestingLog(_entries, _options = {}) {
+    // Später: Rendern nach #testingLogContent
+}
