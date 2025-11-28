1209735565ae7e7ead2fb11eaf42b55972705a8a
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-28 15:26:32 +0100

    Delete packages/creativeLibrary/creativeLibrary.details.js

--- packages/creativeLibrary/creativeLibrary.details.js
+++
@@ -1,7 +0,0 @@
-// packages/creativeLibrary/creativeLibrary.details.js
-// Detail-/Modal-Logik für einzelne Creatives (Stub).
-
-export function openCreativeDetailModal(_creative) {
-    // Wird später mit dem Modal-System verdrahtet.
-    console.debug("[CreativeLibrary] openCreativeDetailModal()");
-}
