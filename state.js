812ca0b5436c21f8c55fe765310e006c5676fbf1
SignalOne.cloud <sebastian.penner.ba@gmail.com>
2025-11-30 22:31:28 +0100

    Update state.js

--- state.js
+++ state.js
@@ -2,7 +2,7 @@ export const META_APP_ID = "732040642590155";
 
 export const META_OAUTH_CONFIG = {
     appId: META_APP_ID,
-    redirectUri: "https://signalone-frontend.onrender.com/",
+    redirectUri: "https://signalone-frontend.onrender.com/meta-auth/",
     scopes: "ads_read,ads_management,business_management"
 };
 
